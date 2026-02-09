f2c50263692144c182c800e06b104b9ef42a21e4
kaanbyzt <kaanbyzt07@gmail.com>
2026-02-09 18:02:30 +0300

    500.1

--- api/index.py
+++ api/index.py
@@ -1,15 +1,10 @@
-import os
-from flask import Flask, render_template
+from flask import Flask
 
-# Klasör yollarını mutlak yol (absolute path) olarak tanımlayalım
-template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
-static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'static'))
-
-app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Kaan Byzt Toolbox - Sunucu Calisiyor!"
 
-# Vercel için kritik satır
+# Bu satır Vercel'in uygulamayı tanıması için hayatidir
 app = app
